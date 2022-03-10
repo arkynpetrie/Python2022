c82938a61269157f40e3e9b0f43e8c7c7f2ff0fa
arkynpetrie <arkpet90@kingshigh.school.nz>
2022-03-11 09:57:48 +1300

    Update conditional.py
    one question to go

--- conditional.py
+++ conditional.py
@@ -1,3 +1,50 @@
+#exercise 7.5
+"""
 pw = input("Enter your password")
 if pw == "secret":
 	print("welcome to the matrix")
+ice_creams = int(input("input how many ice creams's you would like"))
+if ice_creams > 20:  
+  print("we do not have the stock for this amount of ice cream's")
+elif ice_creams <= 20: 
+ print("ok your order is being processed")
+user_miles = int(input("how far are you traveling user in km"))
+if user_miles > 200:
+	print("please fill up your car with gasoline")
+elif user_miles <= 200:
+	print("you have enough gas to travel to your location")
+user_age = int(input("how old are you"))
+if user_age >= 18:
+	print("you are now an adult")
+elif user_age < 18:
+	print("you are still considered a minor")
+user_movie = input("what is your favorite movie")
+if user_movie == "lord of the rings":
+ print("you have great taste")
+elif user_movie != "lord of the rings":
+ print("lord of the rings is the better movie")
+ 
+tale = input("have you heard of the tale of darth plagueis the wise")
+if tale == "yes":
+ print("you are a nerd")
+else:
+	print("look up the tale of darth plagueis")
+director = input("who directed Passion of the Christ")
+if director == "mel gibson":
+	print("correct")
+elif director =="MEL GIBSON":
+	print("correct")
+elif director == "Mel Gibson":
+	print("correct")
+else: 
+	print("Mel Gibson did")
+"""
+score = 0 
+Arkyn_age = int(input("how old is arkyn the great"))
+if Arkyn_age == 14:
+	score == 1
+else:
+	print("you are wrong")
+favorite_movie = input("jigsaw")
+if favorite_movie == jigsaw:
+	score== 2 
