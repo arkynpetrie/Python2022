baea7015a25be5461b798b2fa11f25f13fa75e7b
arkynpetrie <arkpet90@kingshigh.school.nz>
2022-03-17 14:56:36 +1300

    Update game1.py

--- game1.py
+++ game1.py
@@ -1,15 +1,45 @@
 from adventurelib import *
-@when("brush teeth")
-@when("brush")
-@when("clean teeth")
-def brush_teeth():
-	print("you brush your teeth")
-@when("comb hair")
-@when("comb")
-def comb_hair():
-	say("""
-		you brush your long flowing locks with the gold hairbrush that you have selected from the red basket
-		""")
+
+space = Room("""
+	you are drifting in space it feels very cold.
+	a slate-blue spaceship sits completely silently to your left,
+	its airlock open and waiting.
+	""")
+
+spaceship = Room("""
+	The bridge if the spaceship is shiny and white, with thousands
+	 of small, red, blinking lights
+	""")
+current_room = space
+
+@when("enter airlock")
+@when("enter spaceship")
+@when("enter ship")
+def enter_spaceship():
+ global current_room
+if current_room is not space:
+	say("there is no airlock here")
+else:
+	current_room = spaceship
+	print("""you heave yourself into the spaceship and
+	slam you hand on the button to close the door.
+	""")
+	print(current_room)
+
+cargo = "Room1"
+@when("enter cargo")
+@when("enter storage")
+@when("enter cargo hold")
+def enter_cargo():
+ global current_room
+if current_room == space:
+	print("cannot enter cargo please enter airlock to proceed")
+elif current_room == spaceship:
+	print("""you enter the storage area of your ship
+	your foot steps echo as you enter the room""")
+
+
+
 def main():
 	start()
 
